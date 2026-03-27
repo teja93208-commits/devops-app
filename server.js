94e06e170e143feca370d95fc849f83d4d9a5dfe
Tejavardhan999 <teja93208@gmail.com>
2026-03-27 15:50:22 +0530

    Update2

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("DevOps App via Jenkins 🚀");
+});
+
+app.listen(3000, '0.0.0.0', () => console.log("Running on port 3000"));
